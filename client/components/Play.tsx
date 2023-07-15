a3ab68dec19b689288b74fe3e1b19a86cd831930
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-07-15 15:32:09 +1200

    Initial commit

---
+++ client/components/Play.tsx
@@ -0,0 +1,13 @@
+import { AiFillPlayCircle } from 'react-icons/ai'
+
+function Play() {
+  return (
+    <>
+      <button>
+        <AiFillPlayCircle />
+      </button>
+    </>
+  )
+}
+
+export default Play
