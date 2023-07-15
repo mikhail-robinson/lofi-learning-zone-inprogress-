13c95c497eda7d93bc2121bc061c72e5b4ed8f42
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-07-15 17:27:43 +1200

    show song title dynamically

--- client/components/PlayButton.tsx
+++ client/components/PlayButton.tsx
@@ -1,14 +1,42 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { AiFillPlayCircle, AiFillPauseCircle } from 'react-icons/ai'
 import YouTube from 'react-youtube'
 
+// const youtubeAPIKey = process.env.REACT_APP_YOUTUBE_API_KEY
+
 function PlayButton() {
   const [isPlaying, setIsPlaying] = useState(false)
-  const playlistId = 'PL0piECm5VoRHqftdO93lHC34WypyX9akH'
+  const playlistId = 'PLOzDu-MXXLliO9fBNZOQTBDddoA3FzZUo' // Replace with the ID of your YouTube playlist
+  const [currentSong, setCurrentSong] = useState('')
+  const playerRef = useRef(null)
+
+  useEffect(() => {
+    if (isPlaying) {
+      fetchSongTitle()
+    }
+  }, [isPlaying])
+
+  async function fetchSongTitle() {
+    try {
+      const videoId =
+        playerRef.current.getPlaylist()[playerRef.current.getPlaylistIndex()]
+
+      const response = await fetch(
+        `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=AIzaSyD9BrjHAaK853mZhyQC66pnXcL9XUm-fns`,
+      )
+      const data = await response.json()
+      console.log('THis is the API data', data)
+
+      const title = data.items[0].snippet.title
+      setCurrentSong(title)
+    } catch (error) {
+      console.log('Error fetching song information:', error)
+    }
+  }
 
   function handleMusicPlaylist() {
     setIsPlaying((prevState) => !prevState)
+    setCurrentSong('')
   }
 
   const opts = {
@@ -28,7 +56,12 @@ function PlayButton() {
     <>
       {isPlaying ? (
         <>
-          <YouTube opts={opts} />
+          <YouTube
+            opts={opts}
+            className="youtube-player"
+            onPlay={fetchSongTitle}
+            onReady={(event) => (playerRef.current = event.target)}
+          />
           <button onClick={handleMusicPlaylist}>
             <AiFillPauseCircle />
           </button>
@@ -38,6 +71,7 @@ function PlayButton() {
           <AiFillPlayCircle />
         </button>
       )}
+      {currentSong && <p>Current Song: {currentSong}</p>}
     </>
   )
 }
