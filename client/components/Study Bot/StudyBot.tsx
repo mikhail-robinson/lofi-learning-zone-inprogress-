ec7a44f8a6373212d5260e6391e740d224b27353
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-08-14 15:05:28 +1200

    change styling to match AudioParent component

--- client/components/Study Bot/StudyBot.tsx
+++ client/components/Study Bot/StudyBot.tsx
@@ -13,18 +13,36 @@ function StudyBot() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-        />
-        <button className="text-white" type="submit">
-          Ask
-        </button>
-      </form>
-      {answer && <p className="w-1/2 text-center text-white">{answer}</p>}
+    <div className="bg-gray-900 shadow-lg rounded p-3 w-4/5 flex flex-col items-center justify-center py-6 mt-40">
+      <div className="w-full bg-gray-900 rounded bg-opacity-60 flex flex-col items-center justify-center space-y-4">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center space-y-2 w-4/5"
+        >
+          <div className="w-full flex justify-center">
+            <input
+              type="text"
+              placeholder="Ask me a question!"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+              className="p-2 rounded w-3/5 max-w-xs"
+            />
+          </div>
+          <div className="w-1/5">
+            <button
+              className="text-white bg-gray-800 p-2 rounded w-full hover:bg-indigo-700"
+              type="submit"
+            >
+              Ask
+            </button>
+          </div>
+        </form>
+        {answer && (
+          <p className="w-full text-center text-white mt-4 break-words">
+            {answer}
+          </p>
+        )}
+      </div>
     </div>
   )
 }
