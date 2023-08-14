a54e9ea1977fa08957cc55c0129d44ffcd34e37a
Mikhail Robinson <mikhailrobinson0@gmail.com>
2023-08-14 14:10:12 +1200

    add background colour

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -4,13 +4,17 @@ import StudyBot from './Study Bot/StudyBot'
 
 function App() {
   return (
-    <div className='bg-gif h-screen w-screen"'>
-      <h1 className=" flex items-center justify-center text-6xl">Study Zone</h1>
-      <Routes>
-        <Route path="/" element={<AudioParent />} />
-      </Routes>
-      <StudyBot />
-    </div>
+    <main className="grid place-items-center min-h-screen bg-gradient-to-t from-blue-200 to-indigo-900 p-5">
+      <div className="flex flex-col items-center justify-center w-full">
+        <h1 className="text-4xl sm:text-5xl md:text-7xl font-bold text-gray-200 mb-5">
+          Study Zone
+        </h1>
+        <Routes>
+          <Route path="/" element={<AudioParent />} />
+        </Routes>
+        <StudyBot />
+      </div>
+    </main>
   )
 }
 
